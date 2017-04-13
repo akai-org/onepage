b3b6f4ac78432a6c0069978b46844de2866a7d75
dominikbrandon <grzelak.dominik@gmail.com>
2017-04-13 13:18:29 +0200

    added logo, icons, styles of grid and button

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var runSequence = require('run-sequence');
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', function() {
 
     browserSync.init({
         server: "./app"
@@ -21,4 +22,20 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['serve']);
+// copy Font Awesome fonts
+gulp.task('copyfonts', function() {
+   return gulp.src('./bower_components/font-awesome/fonts/**/*.{ttf,woff,woff2,eot,svg}')
+   .pipe(gulp.dest('app/fonts'));
+});
+
+// copy Font Awesome scss
+gulp.task('copyscss', function() {
+   return gulp.src('bower_components/font-awesome/scss/**/*')
+    .pipe(gulp.dest('app/scss'));
+});
+
+gulp.task('default', function(done) {
+    runSequence('copyfonts', 'copyscss', 'sass', 'serve', function() {
+        done();
+    });
+});
