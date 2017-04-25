197b1b6f4807c921a73c4add06c307eaaed4dc4e
herdu <mateusz.dobrowolski71@gmail.com>
2017-04-25 19:09:22 +0200

    main.scss added, index.html includes eventsDetails.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,45 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var fileinclude = require('gulp-file-include');
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['html', 'sass', 'copy'], function() {
 
     browserSync.init({
-        open: 'external',
-        host: 'localhost',
-        proxy: 'localhost/one-page/app/eventsDetails.html'
+
+        server: "./public"
     });
     gulp.watch("app/scss/*.scss", ['sass']);
+    gulp.watch("app/img/*", ['copy']);
     gulp.watch("app/*.html").on('change', browserSync.reload);
+
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
     return gulp.src("app/scss/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest("app/css"))
+        .pipe(gulp.dest("public/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('html', function() {
+  return gulp.src("app/index.html")
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest("public/"));
+});
+
+gulp.task('copy', function() {
+    return gulp.src("app/img/*")
+        .pipe(gulp.dest("public/img"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('default', ['serve']);
+
+
+
