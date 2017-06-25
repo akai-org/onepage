1f62197d0da43f4884cc3e86b965fef16021a6b4
dominikbrandon <grzelak.dominik@gmail.com>
2017-06-25 14:20:01 +0200

    added 'add to calendar' button

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,8 @@
 $( document ).ready(function() {
 
-  @@include('./smooth-scroll.js');
-  @@include('./eventsDetails.js');
-  @@include('./formValidation.js');
+  @@include('./smooth-scroll.js')
+  @@include('./date.js')
+  @@include('./eventsDetails.js')
+  @@include('./formValidation.js')
 
 });
