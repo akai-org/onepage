d480e72041bff8a328feee2ea8fcc8fb6478b06e
Rafał Rudol <rrrrudol@gmail.com>
2017-07-21 21:46:48 +0200

    add injecting files feature

--- gulp/tasks/markup.js
+++ gulp/tasks/markup.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const inject = require('gulp-inject');
+const injectfile = require('gulp-inject-file');
 const htmlmin = require('gulp-htmlmin');
 const htmlhint = require('gulp-htmlhint');
 const strip = require('gulp-strip-comments');
@@ -8,6 +9,7 @@ const paths = require('../config/paths');
 
 gulp.task('markup', function () {
   return gulp.src(paths.source.markup)
+    .pipe(injectfile())
     .pipe(htmlhint())
     .pipe(htmlhint.failReporter())
     .pipe(inject(gulp.src([paths.publish.styles+"/**/*.css", paths.publish.scripts+"/**/*.js"], {read: false}), {ignorePath: 'public', addRootSlash: false}))
@@ -18,6 +20,7 @@ gulp.task('markup', function () {
 
 gulp.task('markup:dev', function () {
   return gulp.src(paths.source.markup)
+    .pipe(injectfile())
     .pipe(htmlhint())
     .pipe(htmlhint.reporter())
     .pipe(inject(gulp.src([paths.build.styles+"/**/*.css", paths.build.scripts+"/**/*.js"], {read: false}), {ignorePath: 'build', addRootSlash: false}))
