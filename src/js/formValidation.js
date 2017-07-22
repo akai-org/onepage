5f85b25eb8658e804a51ae3a1924b43b6718ef50
Rafał Rudol <rrrrudol@gmail.com>
2017-07-22 10:18:37 +0200

    fix scripts

--- src/js/formValidation.js
+++ src/js/formValidation.js
@@ -1,5 +1,5 @@
-function formValidation() {
-  $(':required').one('blur keydown', function() {
-    $(this).addClass('.disable');
-  });
-}
+// function formValidation() {
+//   $(':required').one('blur keydown', function() {
+//     $(this).addClass('.disable');
+//   });
+// }
