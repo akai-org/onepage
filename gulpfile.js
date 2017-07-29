86132780d2a6465d749445f8a86533739b6a2d4d
Tomasz Gil <tomaszgil.pom@gmail.com>
2017-07-29 12:24:17 +0200

    Include watch in default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -88,7 +88,7 @@ gulp.task('build', gulpsync.sync(['styles', 'scripts', 'images', 'copy', 'markup
 
 gulp.task('watch', ['styles:watch', 'scripts:watch', 'images:watch', 'copy:watch', 'markup:watch']);
 
-gulp.task('serve', ['build'], () => {
+gulp.task('serve', ['build', 'watch'], () => {
   browserSync.init({server: paths.dist.dir});
-  gulp.watch([paths.dist.markup, paths.dist.scripts]).on("change", browserSync.reload);
+  gulp.watch([paths.dist.markup, paths.dist.scripts, paths.dist.styles]).on("change", browserSync.reload);
 });
