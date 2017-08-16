1dbfeeeea0db4d749f912499cc181d3302d41961
Tomasz Gil <tomaszgil.pom@gmail.com>
2017-08-17 00:09:09 +0200

    refactor about section

--- src/js/components/about.js
+++ src/js/components/about.js
@@ -3,18 +3,20 @@ const About = (() => {
   const api = {};
 
   api.compile = (data) => {
-    var {title, text, image} = data;
+    const {title, subtitle, text, image} = data;
+    const compiledText = text.map(paragraph => `<p>${paragraph}</p>`).join("\n");
+
     return `
       <section class="about" id="about">
         <div class="container">
          <div class="about-header">
-            <h2>O wydarzeniu</h2>
-            <h1>${title}</h1>
+            <h3>${subtitle}</h3>
+            <h2>${title}</h2>
             <div class="divider"></div>
           </div>
           <div class="about-columns">
             <div class="about-left">
-              <p>${text}</p>
+              ${compiledText}
             </div>
             <div class="about-right">
               <img src="${image.src}" alt="${image.alt}">
