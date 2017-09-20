35556ebe21f2d2994c2565346c4f6d20c4d5991a
Tomasz Gil <tomaszgil.pom@gmail.com>
2017-09-20 23:52:06 +0200

    add basic template html stucture with styling

--- src/js/components/collaborationPricing.js
+++ src/js/components/collaborationPricing.js
@@ -5,13 +5,69 @@ const CollaborationPricing = (() => {
   api.compile = (data) => {
     var {title, text} = data;
     return `
-      <section id="collaboration" class="collaboration-pricing">
-        <div class="container">
-          <h2 class="heading">${title}</h2>
-          <div class="divider"></div>
-          <p class="general-info">${text}</p>
+    <section id="collaboration" class="collaboration-pricing">
+      <div class="container">
+        <h2 class="heading">${title}</h2>
+        <div class="divider"></div>
+        <p class="general-info">${text}</p>
+        <div class="collaboration-plans">
+          <div class="benefits">
+            <p class="heading">Co otrzymujesz?</p>
+            <ul class="benefits-list">
+              <li>A</li>
+              <li>B</li>
+              <li>C</li>
+              <li>D</li>
+              <li>E</li>
+              <li>F</li>
+            </ul>
+          </div>
+          <div class="plan">
+            <div class="heading">
+              <span class="price">200 zł</span>
+              <span class="period">semestr</span>
+            </div>
+            <ul class="plan-content">
+              <li>A</li>
+              <li>B</li>
+              <li>C</li>
+              <li>D</li>
+              <li>E</li>
+              <li>F</li>
+            </ul>
+          </div>
+          <div class="plan">
+            <div class="heading">
+              <span class="price">200 zł</span>
+              <span class="period">semestr</span>
+            </div>
+            <ul class="plan-content">
+              <li>A</li>
+              <li>B</li>
+              <li>C</li>
+              <li>D</li>
+              <li>E</li>
+              <li>F</li>
+            </ul>
+          </div>
+          <div class="plan">
+            <div class="heading">
+              <span class="price">200 zł</span>
+              <span class="period">semestr</span>
+            </div>
+            <ul class="plan-content">
+              <li>A</li>
+              <li>B</li>
+              <li>C</li>
+              <li>D</li>
+              <li>E</li>
+              <li>F</li>
+            </ul>
+          </div>
         </div>
-      </section>
+      </div>
+    </section>
+
     `;
   };
 
