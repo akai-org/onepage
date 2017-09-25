628bd13122b1ff9b4d542ec2503c68918281e0d2
Tomasz Gil <tomaszgil.pom@gmail.com>
2017-09-25 20:16:17 +0200

    resizing table with styling

--- src/js/components/collaborationPricing.js
+++ src/js/components/collaborationPricing.js
@@ -1,3 +1,5 @@
+const $ = require('jquery');
+
 const CollaborationPricing = (() => {
 
   const api = {};
@@ -9,16 +11,18 @@ const CollaborationPricing = (() => {
     const compiledCategories = categories.map(category => `<li>${category}</li>`).join("\n");
     const compiledPlans = plans.map((plan) => {
       const {name, price, period, benefits} = plan;
-      const compiledPlanBenefits = benefits.map(benefit => `<li>${benefit}</li>`).join("\n");
+      const compiledPlanBenefits = benefits.map((benefit) => {
+        return `<li><span class="${benefit ? 'tick' : 'cross'}"></span></li>`
+      }).join("\n");
 
       return `
       <div class="plan">
         <div class="heading">
-          <p class="price">${name}</p>
+          <p class="name">${name}</p>
           <span class="price">${price}</span>
-          <span class="period">${period}</span>
+          <span class="period">/ ${period}</span>
         </div>
-        <ul class="plan-content">
+        <ul class="plan-list">
           ${compiledPlanBenefits}
         </ul>
       </div>
@@ -26,27 +30,65 @@ const CollaborationPricing = (() => {
     }).join("\n");
 
     return `
-    <section id="collaboration" class="collaboration-pricing">
-      <div class="container">
-        <h2 class="heading">${title}</h2>
-        <div class="divider"></div>
-        <p class="general-info">${text}</p>
-        <div class="collaboration-plans">
-          <div class="benefits">
-            <p class="heading">${name}</p>
-            <ul class="benefits-list">
-              ${compiledCategories}
-            </ul>
+      <section id="collaboration" class="collaboration-pricing">
+        <div class="container">
+          <h2 class="heading">${title}</h2>
+          <div class="divider"></div>
+          <p class="general-info">${text}</p>
+          <div class="collaboration-plans">
+            <div class="benefits">
+              <p class="heading">${name}</p>
+              <ul class="benefits-list">
+                ${compiledCategories}
+              </ul>
+            </div>
+            ${compiledPlans}
           </div>
-          ${compiledPlans}
         </div>
-      </div>
-    </section>
+      </section>
     `;
   };
 
-  api.componentReady = () => {
+  const resizeRow = (row) => {
+    let maxHeight = 0;
 
+    for (let j = 0; j < row.length; j++) {
+      let height = $(row[j]).height();
+
+      if (height > maxHeight) {
+        maxHeight = height;
+      }
+    }
+
+    for (let j = 0; j < row.length; j++) {
+      $(row[j]).height(maxHeight);
+    }
+  };
+
+  const resizeCells = () => {
+    const $benefits = $('.benefits-list li');
+    const $plans = $('.plan-list li');
+
+    for (let i = 0; i < $benefits.length; i++) {
+      let row = [];
+      row.push($benefits[i]);
+
+      for (let j = 0; (i + j * $benefits.length) < $plans.length; j++) {
+        row.push($plans[i + j * $benefits.length]);
+      }
+
+      resizeRow(row);
+    }
+  };
+
+  const resizeTable = () => {
+    const $headings = $('.collaboration-plans .heading');
+    resizeRow($headings);
+    resizeCells();
+  };
+
+  api.componentReady = () => {
+    resizeTable();
   };
 
   return {
