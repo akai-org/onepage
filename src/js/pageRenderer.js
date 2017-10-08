5ef5c7e06065f884a7ba77d02ea2a0bff7ba56cf
Rafał Rudol <rrrrudol@gmail.com>
2017-10-08 18:24:55 +0200

    delete gulp

--- src/js/pageRenderer.js
+++ src/js/pageRenderer.js
@@ -41,7 +41,7 @@ const pageRenderer = (() => {
     })
     .done(() => {
       const compiled = compiledPageComponents.join("\n");
-      $("#content").append(compiled);
+      $("body").append(compiled);
       registerComponents(pageComponents);
     })
     .fail(() => {
