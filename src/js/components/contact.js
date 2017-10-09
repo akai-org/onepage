2a5887b35f935f7c778b8f898717a6100d78e21e
Tomasz Gil <tomaszgil.pom@gmail.com>
2017-10-09 21:51:24 +0200

    #52 refactor contact

--- src/js/components/contact.js
+++ src/js/components/contact.js
@@ -4,18 +4,45 @@ const Contact = (() => {
 
   const api = {};
 
+  const heading = (title) => {
+    if (title) {
+      return `
+        <h2 class="contact-heading">${title}</h2>
+        <div class="divider"></div>
+      `;
+    } else {
+      return ''
+    }
+  };
+
+  const copy = (text) => {
+    if (text) {
+      return `<p class="contact-info">${text}</p>`;
+    } else {
+      return '';
+    }
+  };
+
+  const button = (text) => {
+    if (text) {
+      return `<button class="send-btn">${text}</button>`;
+    } else {
+      return '';
+    }
+  };
+
   api.compile = (data) => {
     var {title, text, buttonText} = data;
+
     return `
       <section id="contact" class="contact">
         <div class="container">
-          <h2 class="contact-heading">${title}</h2>
-          <div class="divider"></div>
-          <p class="contact-info">${text}</p>
+          ${ heading(title) }
+          ${ copy(text) }
           <form class="contact-form">
             <input type="email" class="email" placeholder="Adres e-mail" required>
             <textarea class="message" placeholder="Twoja wiadomość..." rows="4"></textarea>
-            <button class="send-btn">${buttonText}</button>
+            ${ button(buttonText) }
           </form>
         </div>
       </section>
