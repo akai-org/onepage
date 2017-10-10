29405ac7cd95de90a307a1730dcdd44e1f9f917b
Haradd <blazej.piaskowski@gmail.com>
2017-10-10 10:23:28 +0200

    move gallery creating into compile

--- src/js/components/gallery.js
+++ src/js/components/gallery.js
@@ -3,20 +3,19 @@ const $ = require("jquery");
 const Gallery = (() => {
 
   const api = {};
-  let photosArray
 
   api.compile = (data) => {
     const {title, photos} = data;
-    photosArray = photos
+    const compiledPhotos = photos.map(photo => `<a href="${photo}" data-lightbox="image"><img src="${photo}" /></a>`).join('');
     return `
       <section class="gallery" id="gallery">
         <div class="container">
           <h1 class="gallery-heading">${title}</h1>
-
+          <div class="gallery-slider image-set">
+            ${compiledPhotos}
           </div>
         </div>
       </section>
-
     `;
   };
 
@@ -24,31 +23,24 @@ const Gallery = (() => {
     const fadingSpeed = 1000;
     const pause = 4000;
     let interval;
-    let $container = $('#gallery .container');
-    let $slider = $('<div class="gallery-slider image-set">');
-
-    photosArray.forEach(photo => {
-      $slider.append(`<a href="${photo}" data-lightbox="image"><img src="${photo}" /></a>`)
-    });
-    $container.append($slider);
+    let $slider = $('#gallery .container .gallery-slider');
 
     function startSlider() {
         interval = setInterval( () => {
           //insert first image after last
-          let $img = $('.gallery-slider a', $container);
+          let $img = $('a', $slider);
           $img.fadeOut(fadingSpeed, () => {
             $img.first().insertAfter($img.last());
           });
-
           $img.fadeIn(fadingSpeed);
         }, pause);
-    }
+    };
 
     function pauseSlider() {
         clearInterval(interval);
-    }
+    };
 
-    $('.gallery-slider')
+    $slider
         .mouseenter(pauseSlider)
         .mouseleave(startSlider);
 
