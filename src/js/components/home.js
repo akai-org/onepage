1f9f0eec4a608d8a8031760f7d666eaf14fa836f
Tomasz Gil <tomaszgil.pom@gmail.com>
2017-10-11 15:37:26 +0200

    #52 refactor home section

--- src/js/components/home.js
+++ src/js/components/home.js
@@ -2,6 +2,72 @@ const Home = (() => {
 
   const api = {};
 
+  const headingTitle = (title) => {
+    if (title) {
+      return `
+        <h1 class="page-title">${title}</h1>
+        <div class="divider"></div>
+      `;
+    } else {
+      return '';
+    }
+  };
+
+  const headingSubtitle = (subtitle) => {
+    if (subtitle) {
+      return `
+        <h2 class="page-subtitle">${subtitle}</h2>
+      `;
+    } else {
+      return '';
+    }
+  };
+
+  const headingDescription = (text) => {
+    if (text) {
+      return `
+        <p class="page-description">${text}</p>
+      `;
+    } else {
+      return '';
+    }
+  };
+
+  const button = (btn, cssClass) => {
+    if (btn === undefined) {
+      return '';
+    }
+
+    const {link, text} = btn;
+    if (link && text) {
+      return `
+        <a href="${link}" class="${cssClass}">${text}</a>
+      `;
+    } else {
+      return '';
+    }
+  };
+
+  const scroll = (btn) => {
+    if (btn === undefined) {
+      return '';
+    }
+
+    const {link, text} = btn;
+    if (link && text) {
+      return `
+        <a class="home-scroll" href="${link}">
+          <div class="home-icon-container">
+            <div class="home-scroll-description">${text}</div>
+            <i class="fa fa-chevron-down" aria-hidden="true"></i>
+          </div>
+        </a>
+      `;
+    } else {
+      return '';
+    }
+  };
+
   api.compile = (data) => {
     var {title, subtitle, description, primaryButton, seconadryButton, scrollButton} = data;
 
@@ -10,21 +76,15 @@ const Home = (() => {
         <div class="container">
           <div class="home-topbar">
             <img src="img/logo.svg" alt="Logo" class="home-logo">
-            <a href="${seconadryButton.link}" class="button button-alt">${seconadryButton.text}</a>
+            ${ button(seconadryButton, 'button button-alt') }
           </div>
           <div class="home-title">
-            <h2 class="page-subtitle">${subtitle}</h2>
-            <h1 class="page-title">${title}</h1>
-            <div class="divider"></div>
-            <p class="page-description">${description}</p>
-            <a href="${primaryButton.link}" class="button">${primaryButton.text}</a>
+            ${ headingSubtitle(subtitle) }
+            ${ headingTitle(title) }
+            ${ headingDescription(description) }
+            ${ button(primaryButton, 'button') }
           </div>
-          <a class="home-scroll" href="${scrollButton.link}">
-            <div class="home-icon-container">
-              <div class="home-scroll-description">${scrollButton.text}</div>
-              <i class="fa fa-chevron-down" aria-hidden="true"></i>
-            </div>
-          </a>
+          ${ scroll(scrollButton) }
         </div>
       </section>
     `;
