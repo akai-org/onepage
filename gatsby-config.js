f1b2271ca49ebe486a99969e0d1b4ebe15213359
Rafał Rudol <rrrrudol@gmail.com>
2018-05-25 10:59:39 +0200

    add sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,13 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        precision: 8,
+      },
+    },
+  ],
 };
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './style.scss'
 
 const Header = () => (
   <div className="home-topbar">
     <img src="https://github.com/akai-org/akai-assets/blob/master/public/png/logo.png?raw=true" alt="Logo" className="home-logo" />  
-    <Link to="/About/" className="button button-alt">O nas</Link>
-    <Link to="/contact" className="button button-alt">Kontakt</Link>
+    <div className="home-topbar-right">
+      <Link to="/About/" className="button button-alt">O nas</Link>
+      <Link to="/contact" className="button button-alt">Kontakt</Link>
+    </div>
   </div>
 )
 
